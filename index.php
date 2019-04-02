daf9bf54763d2ae8c83e4dea37472ac3e7d75d76
tom2kk <tlparker1988@gmail.com>
2019-04-02 14:04:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>jQuery.parseXML demo</title>
-  <script src="fetch-feed.php"></script>
+  <title>Live Football Feed</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
